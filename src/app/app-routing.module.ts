42b9ecb6679be06a0bca7d50336d02eb01d469f3
laurakessas <32477876+laurakessas@users.noreply.github.com>
2020-04-20 18:43:11 +0200

    gestion page detail pizza

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  {
-    path: 'detail-pizza',
-    loadChildren: () => import('./pages/detail-pizza/detail-pizza.module').then( m => m.DetailPizzaPageModule)
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
+  {
+    path: 'detail-pizza/:id',
+    loadChildren: () => import('./pages/detail-pizza/detail-pizza.module').then(m => m.DetailPizzaPageModule)
   },
 
 ];
--- src/app/pages/detail-pizza/detail-pizza.page.ts
+++ src/app/pages/detail-pizza/detail-pizza.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import PizzaDto from 'src/app/models/pizza.dto';
+import { ActivatedRoute } from '@angular/router';
+import { PizzaService } from 'src/app/services/pizza.service';
+import { loadavg } from 'os';
+import { async } from 'rxjs/internal/scheduler/async';
 
 @Component({
   selector: 'app-detail-pizza',
@@ -6,10 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detail-pizza.page.scss'],
 })
 export class DetailPizzaPage implements OnInit {
-
-  constructor() { }
+  pizza: PizzaDto;
+  pizzaId: number;
+  constructor(private route: ActivatedRoute, private pizzaService: PizzaService, ) { }
 
   ngOnInit() {
+    this.route.params.subscribe((params) => {
+      this.pizzaId = params.id;
+      console.log("DetailPizzaPage -> ngOnInit -> pizzaId", this.pizzaId)
+      this.load();
+
+    })
   }
+
+  async load() {
+    this.pizza = await this.pizzaService.getOne(this.pizzaId).toPromise();
+    console.log("pizza", this.pizza);
+
+
+  }
+
+
+
+
 
 }
